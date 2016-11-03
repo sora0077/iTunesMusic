1d2aa9b4c17d79287d622a24bbe45a283ac08790
t_hayashi <t.hayashi0077+github@gmail.com>
2016-11-03 23:32:14 +0900

    First implementation player using AbstractPlayerKit.Player

--- Sources/Player/CrossFadePlayer.swift
+++ Sources/Player/CrossFadePlayer.swift
@@ -9,21 +9,168 @@
 import Foundation
 import AVKit
 import AVFoundation
+import APIKit
 import RxSwift
 import ErrorEventHandler
 import RealmSwift
+import AbstractPlayerKit
 
 
-private class PlayerItem {
+private class PlayerItem: AbstractPlayerKit.PlayerItem {
 
 }
 
 private final class PlayerTrackItem: PlayerItem {
 
+    private let track: Model.Track
+
+    override var state: State {
+        didSet {
+            print("track", state)
+            if case .requesting = state {
+                fetch()
+            }
+        }
+    }
+
+    init(track: Model.Track) {
+        self.track = track
+
+        super.init()
+    }
+
+    fileprivate func fetch(_ completion: @escaping (State) -> Void = { _ in }) {
+        guard let track = track.entity, track.canPreview else {
+            state = .rejected
+            completion(state)
+            return
+        }
+
+        if (track.metadata?.fileURL != nil) || (track.metadata?.previewURL != nil) {
+            state = .readyForPlay
+            completion(state)
+            return
+        }
+        let id = track.id
+        Session.shared.send(GetPreviewUrl(id: id, url: track.viewURL), callbackQueue: callbackQueue) { [weak self] result in
+            switch result {
+            case .success(let (url, duration)):
+                let realm = iTunesRealm()
+                try? realm.write {
+                    guard let track = realm.object(ofType: _Track.self, forPrimaryKey: id) else { return }
+                    let metadata = _TrackMetadata(track: track)
+                    metadata.updatePreviewURL(url)
+                    metadata.duration = Double(duration) / 1000
+                    realm.add(metadata, update: true)
+                }
+                self?.state = .readyForPlay
+                self?.isRequestFinished = true
+                completion(.readyForPlay)
+            case .failure:
+                self?.state = .rejected
+                completion(.rejected)
+            }
+        }?.resume()
+    }
+
+    override func generateAVPlayerItem(_ completion: @escaping (AVPlayerItem) -> Void) {
+        guard let track = track.entity, track.canPreview else {
+            state = .rejected
+            return
+        }
+        guard let url = track.metadata?.fileURL ?? track.metadata?.previewURL else {
+            state = .rejected
+            return
+        }
+
+        let asset = AVURLAsset(url: url)
+        asset.loadValuesAsynchronously(forKeys: ["duration"]) {
+            let item = AVPlayerItem(asset: asset)
+            completion(item)
+        }
+    }
 }
 
 private final class PlayerListItem: PlayerItem {
 
+    override var state: State {
+        didSet {
+            print("playlist", state)
+            if case .requesting = state {
+                fetch()
+            }
+        }
+    }
+
+    private let playlist: Playlist
+
+    private var caret: Int = 0
+    private var items: [PlayerTrackItem] = []
+
+    init(playlist: Playlist) {
+        self.playlist = playlist
+
+        super.init()
+    }
+
+    private func fetch() {
+        guard doOnMainThread(execute: self.fetch()) else { return }
+
+        if playlist.isTrackEmpty || playlist.trackCount <= caret {
+            state = .rejected
+            return
+        }
+        if let paginator = playlist as? _Fetchable,
+            !paginator._hasNoPaginatedContents && playlist.trackCount - caret < 3 {
+            if !paginator._requesting.value {
+                paginator.fetch(ifError: DefaultError.self, level: DefaultErrorLevel.none) { [weak self] error in
+                    self?.state = .prepareForRequest
+                }
+            }
+            return
+        }
+        let track = Model.Track(track: playlist.track(at: caret))
+        let item = PlayerTrackItem(track: track)
+        print(caret, track.entity?.name ?? "")
+        items[safe: caret] = item
+        item.fetch { [weak self] state in
+            if case .readyForPlay = state {
+                self?.state = .readyForPlay
+            }
+        }
+    }
+
+    override func generateAVPlayerItem(_ completion: @escaping (AVPlayerItem) -> Void) {
+        defer {
+            caret += 1
+            state = .prepareForRequest
+        }
+        if let item = items[safe: caret] {
+            item.generateAVPlayerItem(completion)
+        }
+    }
+}
+
+private extension Array {
+    subscript (safe index: Int) -> Element? {
+        set {
+            if indices.contains(index) {
+                if let val = newValue {
+                    self[index] = val
+                } else {
+                    remove(at: index)
+                }
+            } else if let val = newValue {
+                append(val)
+            }
+        }
+        get {
+            if indices.contains(index) {
+                return self[index]
+            }
+            return nil
+        }
+    }
 }
 
 private enum DefaultError: ErrorLog.Error {
@@ -39,6 +186,11 @@ private enum DefaultErrorLevel: ErrorLog.Level {
 }
 
 final class Player2: NSObject {
+
+    fileprivate let core: AbstractPlayerKit.Player<PlayerItem>
+
+    fileprivate var items: [PlayerItem] = []
+
     var errorType: ErrorLog.Error.Type = DefaultError.self
 
     var errorLevel: ErrorLog.Level = DefaultErrorLevel.none
@@ -57,6 +209,39 @@ final class Player2: NSObject {
 
     var playing: Bool = false
 
+    private let queuePlayer = AVQueuePlayer()
+
+    override init() {
+        core = AbstractPlayerKit.Player<PlayerItem>(core: queuePlayer)
+        super.init()
+
+        #if (arch(i386) || arch(x86_64)) && os(iOS)
+            queuePlayer.volume = 0.02
+            print("simulator")
+        #else
+            print("iphone")
+        #endif
+        queuePlayer.addObserver(self, forKeyPath: #keyPath(AVQueuePlayer.status), options: .new, context: nil)
+    }
+
+    deinit {
+        queuePlayer.removeObserver(self, forKeyPath: #keyPath(AVQueuePlayer.status))
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard let keyPath = keyPath else { return }
+
+        switch keyPath {
+        case #keyPath(AVQueuePlayer.status):
+            switch queuePlayer.status {
+            case .readyToPlay:
+                queuePlayer.play()
+            default:()
+            }
+        default:()
+        }
+    }
+
     func install(middleware: PlayerMiddleware) {
 
     }
@@ -74,11 +259,13 @@ final class Player2: NSObject {
     }
 
     func add(track: Model.Track) {
-
+        let item = PlayerTrackItem(track: track)
+        core.insert(atFirst: item)
     }
 
     func add(playlist: Playlist) {
-
+        let item = PlayerListItem(playlist: playlist)
+        core.insert(item, after: nil)
     }
 
     func removeAll() {
